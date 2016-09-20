441e60cfdbcef705cd5d7c41a2977d012f63cc40
Luca Hagel <hagel.luca@gmail.com>
2016-09-20 12:29:06 -0700

    Implement single day forecast

--- MakeWeather-starter/ViewController.swift
+++ MakeWeather-starter/ViewController.swift
@@ -11,23 +11,53 @@ import YWeatherAPI
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var locationTextfield: UITextField!
+    
+    var location = "San Francisco"
+    
+    @IBAction func setLocationButton(_ sender: AnyObject) {
+        if let newLocation = locationTextfield.text {
+            location = newLocation
+            self.getWeatherData(location: location)
+        }
+    }
+    @IBAction func show10DayButton(_ sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.getWeatherData(location: location)
+    }
+    
+    func getWeatherData(location: String) {
+        YWeatherAPI.sharedManager().todaysForecast(forLocation: location,
+                                                   success: { (result: [AnyHashable: Any]?) in
+                                                    print(result)
+                                                    self.showWeatherData(data: result!)
+            },
+                                                   failure: { (response: Any?, error: Error?) in
+                                                    print(error)
+            }
+        )
+    }
+    
+    func showWeatherData(data: [AnyHashable: Any]) {
+        let minTemp:String = data["lowTemperatureForDay"] as! String
+        let maxTemp:String = data["highTemperatureForDay"] as! String
+        let description:String = data["shortDescription"] as! String
+        
+        minTempLabel.text = minTemp + "°"
+        maxTempLabel.text = maxTemp + "°"
+        descLabel.text = description
+    }
 
-    YWeatherAPI.sharedManager().todaysForecast(forLocation: "San Francisco",
-                                               success: { (result: [AnyHashable: Any]?) in
-                                                print(result) },
-                                               failure: { (response: Any?, error: Error?) in
-                                                print(error)
-      }
-    )
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
